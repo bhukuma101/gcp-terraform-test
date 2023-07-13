3a12907b874cc80274019196fd7531bd5a2d291e
bhukuma101 <127969551+bhukuma101@users.noreply.github.com>
2023-07-13 22:07:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,15 @@
-provider "google" {
-  project     = "devops-392615"
-  #credentials = file("tcb-project-371706-b114ce01e529.json")
-  region      = "us-east1"
-
-}
-
-resource "google_compute_network" "vpc1" {
-  name                    = "my-custom-network-1"
-  auto_create_subnetworks = "false"
-
-}
-
-resource "google_compute_subnetwork" "my-custom-subnet1" {
-  name          = "my-custom-subnet-1"
-  ip_cidr_range = "10.255.196.0/24"
-  network       = google_compute_network.vpc1.name
-  region        = "us-east1"
-}
+  resource "google_compute_instance" "vm_instance" {
+    name         = "gcptutorials-vm"
+    machine_type = "f1-micro"
+  
+    boot_disk {
+      initialize_params {
+        image = "debian-cloud/debian-9"
+      }
+    }    
+    network_interface {       
+      network = "default"
+      access_config {
+      }
+    }
+  }
