c1766756fa74c2e22a19a65c0b736d717e19e11f
bhukuma101 <127969551+bhukuma101@users.noreply.github.com>
2023-07-13 22:38:52 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "google_compute_instance" "example" {
+resource "google_compute_instance" "default" {
   name         = "example-vm"
   machine_type = "n1-standard-1"
   zone         = "us-central1-a"
