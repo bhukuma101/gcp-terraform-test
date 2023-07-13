b1144a1818292561bca7933af7526d0ade4a244b
bhukuma101 <127969551+bhukuma101@users.noreply.github.com>
2023-07-13 22:09:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,15 +1,22 @@
-  resource "google_compute_instance" "vm_instance" {
-    name         = "gcptutorials-vm"
-    machine_type = "f1-micro"
-  
-    boot_disk {
-      initialize_params {
-        image = "debian-cloud/debian-9"
-      }
-    }    
-    network_interface {       
-      network = "default"
-      access_config {
-      }
+resource "google_compute_instance" "example" {
+  name         = "example-vm"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-1604-lts"
     }
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Include this section to give the VM an external IP address
+    }
+  }
+
+  metadata = {
+    foo = "bar"
   }
