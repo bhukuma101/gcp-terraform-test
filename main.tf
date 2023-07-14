9d1b52fd9f86b8bb251840ffb34ec180f958b1cf
bhukuma101 <127969551+bhukuma101@users.noreply.github.com>
2023-07-14 21:54:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,46 +1,19 @@
-resource "google_service_account" "test1" {
-  account_id   = "jenkins"
-  display_name = "jenkins"
+provider "google" {
+  project     = "devops-392615"
+  credentials = file("/home/bhupendra/devops-cred.json")
+  region      = "us-east1"
+
 }
 
-resource "google_compute_instance" "test1"  {
-  name         = "test"
-  machine_type = "e2-medium"
-  zone         = "us-central1-a"
+resource "google_compute_network" "vpc1" {
+  name                    = "my-custom-network-1"
+  auto_create_subnetworks = "false"
 
-  tags = ["foo", "bar"]
+}
 
-  boot_disk {
-    initialize_params {
-      image = "debian-cloud/debian-11"
-      labels = {
-        my_label = "value"
-      }
-    }
-  }
-
-  // Local SSD disk
-  scratch_disk {
-    interface = "SCSI"
-  }
-
-  network_interface {
-    network = "default"
-
-    access_config {
-      // Ephemeral public IP
-    }
-  }
-
-  metadata = {
-    foo = "bar"
-  }
-
-  metadata_startup_script = "echo hi > /test.txt"
-
-  service_account {
-    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    email  = jenkins@devops-392615.iam.gserviceaccount.com
-    scopes = ["cloud-platform"]
-  }
+resource "google_compute_subnetwork" "my-custom-subnet1" {
+  name          = "my-custom-subnet-1"
+  ip_cidr_range = "10.255.196.0/24"
+  network       = google_compute_network.vpc1.name
+  region        = "us-east1"
 }
